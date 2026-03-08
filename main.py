982e0bdd2a4daf089e01b1ad37eb5b97a645fdeb
piieeto <patsei3399@pm.me>
2026-03-08 14:07:23 +0100

    edit main.py

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
             pass
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
